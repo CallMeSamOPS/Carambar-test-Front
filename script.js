f360e82944ca15c002751ca44aa60055b008b3a9
Joseph-Alexandre Samuel <samuelja1@outlook.fr>
2026-01-20 11:57:35 +0100

    lier front a l'API

---
+++ script.js
@@ -0,0 +1,9 @@
+const API_URL = 'https://carambar-test-back-2.onrender.com/';
+
+document.getElementById('button').addEventListener('click', async () => {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    document.getElementById('question').textContent = data.question;
+    document.getElementById('answer').textContent = data.answer;
+})
