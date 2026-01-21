058c02d21b9d2b09086a380318e8b78be1f627cc
Joseph-Alexandre Samuel <samuelja1@outlook.fr>
2026-01-21 16:14:57 +0100

    Update script.js
    Vérification erreur fetch

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const API_URL = 'https://carambar-test-back-4.onrender.com';
+const API_URL = 'https://carambar-test-back-4.onrender.com/api/v1/jokes/random';
 
 document.getElementById('button').addEventListener('click', async () => {
-    const res = await fetch(API_URL);
-    const data = await res.json();
+    try{
+        const res = await fetch(API_URL);
 
-    document.getElementById('question').textContent = data.question;
-    document.getElementById('answer').textContent = data.answer;
-})
+        const text = await res.text();
+        console.log('reponse brute:', text)
+
+        const data = JSON.parse(text);
+
+        document.getElementById('question').textContent = data.question;
+        document.getElementById('answer').textContent = data.answer;
+    } catch (err){
+        console.error('fetch error:', err);
+    }
+});
