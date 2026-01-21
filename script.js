fbd1f3cf23445c44a0bd357dc19a7796e22a17d5
Joseph-Alexandre Samuel <samuelja1@outlook.fr>
2026-01-21 22:04:06 +0100

    add post button and script

--- script.js
+++ script.js
@@ -15,3 +15,43 @@ document.getElementById('button').addEventListener('click', async () => {
         console.error('fetch error:', err);
     }
 });
+
+const ADD_API_URL = 'https://carambar-test-back-4.onrender.com/api/v1/jokes';
+
+document.getElementById('add-btn').addEventListener('click', async () => {
+
+    const question = document.getElementById('new-question').value;
+    const answer = document.getElementById('new-answer').value;
+    const message = document.getElementById('message');
+
+    if(!question || !answer){
+        message.textContent = 'form need to be completed';
+        return;
+    }
+
+    try {
+        const res = await fetch(ADD_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ question, answer })
+        });
+        const data = await res.json();
+
+        if (!res.ok){
+            message.textContent = 'Add Error';
+            return;
+        }
+        
+        message.textContent = 'Joke added';
+
+        //rest champs
+        document.getElementById('new-question').value ='';
+        document.getElementById('new-answer').value ='';
+
+        } catch (err){
+            message.textContent = 'network error';
+            console.error(err);
+        }
+    });
